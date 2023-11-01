203a0043d61f8ef5502840c955277eba0d08223b
Harman Mann <harmany@hotmail.ca>
2023-10-31 21:57:43 -0400

    youtube video completed

---
+++ assets/js/youtube-content.js
@@ -0,0 +1,17 @@
+// youtube-content.js
+// Wait for the page to be fully loaded
+$(document).ready(function () {
+    // Set the YouTube video ID
+    var videoId = '4jTvXzllE8s';
+
+    // Construct the YouTube embed URL
+    var embedUrl = 'https://www.youtube.com/embed/' + videoId + '?autoplay=1';
+
+    // Create the iframe element
+    var iframe = $('<iframe width="100%" height="500" src="' + embedUrl + '" frameborder="0" allowfullscreen></iframe>');
+
+    // Append the iframe to the video-container after the specified delay
+    setTimeout(function () {
+        $('#video-container').html(iframe);
+    }, );
+});
