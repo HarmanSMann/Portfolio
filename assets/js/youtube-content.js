598c8989ed0c458db7df164d707a4c773ebcd307
Harman Mann <harmany@hotmail.ca>
2023-11-07 17:02:50 -0500

    autoplay off

--- assets/js/youtube-content.js
+++ assets/js/youtube-content.js
@@ -2,11 +2,10 @@
 // Wait for the page to be fully loaded
 $(document).ready(function () {
     // Set the YouTube video ID
-    //<iframe width="1370" height="615" src="https://www.youtube.com/embed/4jTvXzllE8s" title="Video Resume - Take 2 (its complete, I think?)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
     var videoId = '4jTvXzllE8s';
 
     // Construct the YouTube embed URL
-    var embedUrl = 'https://www.youtube.com/embed/' + videoId;
+    var embedUrl = 'https://www.youtube.com/embed/' + videoId + '?autoplay=0';
 
     // Create the iframe element
     var iframe = $('<iframe width="100%" height="500" src="' + embedUrl + '" frameborder="0" allowfullscreen></iframe>');
